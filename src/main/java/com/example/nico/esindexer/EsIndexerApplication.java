4eb3b5cb036f7ec2b0e95d124f50733fcf1d99c3
Nicolas Benizri <nico783@hotmail.com>
2020-12-12 14:09:55 +0100

    [init] es indexer project

---
+++ src/main/java/com/example/nico/esindexer/EsIndexerApplication.java
@@ -0,0 +1,13 @@
+package com.example.nico.esindexer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsIndexerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsIndexerApplication.class, args);
+	}
+
+}
